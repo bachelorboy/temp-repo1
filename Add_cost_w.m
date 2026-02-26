98db29ce7879717b18008bdab568cf02d04f8fc4
bachelorboy <43881971+bachelorboy@users.noreply.github.com>
2026-02-26 12:28:34 +0800

    Delete Add_cost_w.m

--- Add_cost_w.m
+++
@@ -1,9 +0,0 @@
-%-------Yalmip 调用Cplex求解IES经济调度---------%
-%碳排放惩罚费用
-
-WCost=0;
-for t=1:24
-    R_w(t)=0.55*sum(GT_P(:,t))+0.35*sum(GB_Q(:,t))+0.45*P_buy(t)-0.7*P2G_P(t);
-    WCost=WCost+0.21*R_w(t);
-end
-cost=cost+WCost;
